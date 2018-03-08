eb4f6100110476decdc4e22d89f57657c3c2c8ee
tpageforfunzies <taylor.jay.page@gmail.com>
2018-03-07 19:40:45 -0500

    added test for raising exceptions

--- unit_test.py
+++ unit_test.py
@@ -1,13 +1,26 @@
 from test import is_palindrome
+import pytest
 
 
 def increment(x):
     return x + 1
 
 
+def f():
+    raise SystemExit(1)
+
+
+# testing function in this file
 def test_answer():
     assert increment(4) == 5
 
 
+# testing function from other hallway
 def test_palindrome():
     assert is_palindrome("racecar")
+
+
+# testing exception raising using the pytest class
+def test_f():
+    with pytest.raises(SystemExit):
+        f()
