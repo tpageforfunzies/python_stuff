2cac17c4ad9f2c3243b6a8b31e23d63b22fc7b60
tpage <tpage@softlayer.local>
2018-04-10 13:24:39 -0500

    refactored, slightly DRYer

--- flip.py
+++ flip.py
@@ -12,33 +12,39 @@ def printAllScreen():
             print("[%d] %s (%s)"%(i,device.DeviceString,device.DeviceName));
             i = i+1;
         except:
+            print("[100]: Fix All Monitors")
+            print("[200]: Flip All Monitors")
             break;
     return i
+
+def doAll(action):
+    for device in devices:
+        dm = win32.EnumDisplaySettings(device.DeviceName,win32con.ENUM_CURRENT_SETTINGS)
+        if(action == "fix"):
+            dm.DisplayOrientation = win32con.DMDO_DEFAULT
+        elif(action == "flip"):
+            dm.DisplayOrientation = win32con.DMDO_180
+        dm.PelsWidth, dm.PelsHeight = dm.PelsHeight, dm.PelsWidth
+        dm.Fields = dm.Fields & win32con.DM_DISPLAYORIENTATION
+        win32.ChangeDisplaySettingsEx(device.DeviceName,dm)
+
+def flipOne(which):
+    device = win32.EnumDisplayDevices(None,which);
+    print("Rotate device %s (%s)"%(device.DeviceString,device.DeviceName));
+    dm = win32.EnumDisplaySettings(device.DeviceName,win32con.ENUM_CURRENT_SETTINGS)
+    dm.DisplayOrientation = win32con.DMDO_180
+    dm.PelsWidth, dm.PelsHeight = dm.PelsHeight, dm.PelsWidth
+    dm.Fields = dm.Fields & win32con.DM_DISPLAYORIENTATION
+    win32.ChangeDisplaySettingsEx(device.DeviceName,dm)
 
 screen_count=printAllScreen()
 x = int(input("\nEnter a display number [0-%d]: "%screen_count))
 
 if (x == 100):
-    for device in devices:
-        dm = win32.EnumDisplaySettings(device.DeviceName,win32con.ENUM_CURRENT_SETTINGS)
-        dm.DisplayOrientation = win32con.DMDO_DEFAULT
-        dm.PelsWidth, dm.PelsHeight = dm.PelsHeight, dm.PelsWidth
-        dm.Fields = dm.Fields & win32con.DM_DISPLAYORIENTATION
-        win32.ChangeDisplaySettingsEx(device.DeviceName,dm)
+    doAll("fix")
 
 if (x == 200):
-    for device in devices:
-        dm = win32.EnumDisplaySettings(device.DeviceName,win32con.ENUM_CURRENT_SETTINGS)
-        dm.DisplayOrientation = win32con.DMDO_180
-        dm.PelsWidth, dm.PelsHeight = dm.PelsHeight, dm.PelsWidth
-        dm.Fields = dm.Fields & win32con.DM_DISPLAYORIENTATION
-        win32.ChangeDisplaySettingsEx(device.DeviceName,dm)
+    doAll("flip")
 
-device = win32.EnumDisplayDevices(None,x);
-print("Rotate device %s (%s)"%(device.DeviceString,device.DeviceName));
-
-dm = win32.EnumDisplaySettings(device.DeviceName,win32con.ENUM_CURRENT_SETTINGS)
-dm.DisplayOrientation = win32con.DMDO_180
-dm.PelsWidth, dm.PelsHeight = dm.PelsHeight, dm.PelsWidth
-dm.Fields = dm.Fields & win32con.DM_DISPLAYORIENTATION
-win32.ChangeDisplaySettingsEx(device.DeviceName,dm)
+else:
+    flipOne(x)
